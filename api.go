470bea13ae7c2e19e6219e36772710429f9b8bd7
RuChik152 <chernovn152@gmail.com>
2024-08-02 10:00:11 +0300

    add create dir

--- api.go
+++ api.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"log"
-	"sync"
 
 	"github.com/joho/godotenv"
 )
-
-
 
 //  = map[string]string{
 // 	//"CHN":  "resource_map_CHN.json",
@@ -28,26 +25,29 @@ func init() {
 		log.Fatal("Error loading .env file")
 	}
 
+	createGeneralDir()
+	getJson()
+
 }
 
 func main() {
-	runner()
+	// runner()
 }
 
-func runner() {
+// func runner() {
 
-	var wg sync.WaitGroup
+// 	var wg sync.WaitGroup
 
-	semaphore := make(chan struct{}, 1)
-	for i, v := range langList {
-		wg.Add(1)
-		semaphore <- struct{}{}
-		go func(v string, i string) {
-			defer wg.Done()
-			defer func() { <-semaphore }()
-			parseData(v, i)
-		}(v, i)
-	}
+// 	semaphore := make(chan struct{}, 1)
+// 	for i, v := range langList {
+// 		wg.Add(1)
+// 		semaphore <- struct{}{}
+// 		go func(v string, i string) {
+// 			defer wg.Done()
+// 			defer func() { <-semaphore }()
+// 			parseData(v, i)
+// 		}(v, i)
+// 	}
 
-	wg.Wait()
-}
+// 	wg.Wait()
+// }
--- lib.go
+++ lib.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path"
+	"regexp"
+	"strings"
 )
 
 var langList map[string]string
@@ -13,14 +17,60 @@ func getJson() {
 		log.Fatal("lang file not found")
 	}
 
+	re := regexp.MustCompile(`^.*_.*_\.json$`)
+	reName := regexp.MustCompile(`_.*_`)
+
 	for i := 0; i < len(files); i++ {
-		name := getName(files[i].Name())
-		
-		
+		name := files[i].Name()
+
+		if re.MatchString(name) {
+			log.Println("Соотвествует", name)
+
+			log.Printf("%q\n", reName.FindString(name))
+			log.Println(strings.ReplaceAll(reName.FindString(name), "_", ""))
+
+			createDir(strings.ReplaceAll(reName.FindString(name), "_", ""))
+
+		} else {
+			log.Println("Не соотвествует", name)
+		}
+
 	}
 
 }
 
-func getName(fileName string) string {
-	
+func createGeneralDir() {
+	_, errEr := os.Stat("error")
+	if errEr != nil && os.IsNotExist(errEr) {
+		if err := os.Mkdir("error", 0750); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	_, errOut := os.Stat("output")
+	if errOut != nil && os.IsNotExist(errOut) {
+		if err := os.Mkdir("output", 0750); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func createDir(nameDir string) {
+
+	list := []string{
+		"error",
+		"output",
+	}
+
+	// path := path.Join(".", "error", nameDir)
+	// if err := os.Mkdir(path, 0750); err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	for i := 0; i < len(list); i++ {
+		path := path.Join(".", list[i], nameDir)
+		if err := os.Mkdir(path, 0750); err != nil {
+			log.Println(err)
+		}
+	}
 }
